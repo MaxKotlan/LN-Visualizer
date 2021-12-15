9011fe54a936c6be62586b5838fb9ea5b43d7930
Max Kotlan <max.maxkot@gmail.com>
2021-12-14 21:07:15 -0500

    network info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NetworkInfoComponent } from './components/network-info/network-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NetworkInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/network-info/network-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-network-info',
+  templateUrl: './network-info.component.html',
+  styleUrls: ['./network-info.component.scss']
+})
+export class NetworkInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
