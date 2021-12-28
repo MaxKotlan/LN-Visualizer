2880644465c6aa192d23aa90c17ca671431c5b3f
Max Kotlan <max.maxkot@gmail.com>
2021-12-28 00:05:14 -0500

    first attempt for now

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,8 @@ import config from 'config';
 import * as lightning from 'lightning';
 import { LndAuthenticationWithMacaroon } from "lightning";
 import cors from "cors";
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
+import { BehaviorSubject, filter, firstValueFrom, startWith, take } from 'rxjs';
 
 const wss = new WebSocketServer({ port: 8090 });
 
@@ -12,26 +13,30 @@ const lnService = require('ln-service');
 
 console.log(config.get('macaroon'))
 
-
 const macaroonFromConfig: LndAuthenticationWithMacaroon = config.get('macaroon');
 const {lnd} = lightning.authenticatedLndGrpc(macaroonFromConfig);
   
 const app = express();
 app.use(cors());
-let counter = 0;
 
-app.get( "/", async ( req: any, res: any ) => {
-    const {channels, nodes} = await lnService.getNetworkGraph({lnd});
+let networkGraphSubject$: BehaviorSubject<any> = new BehaviorSubject({});
 
-    const filteredView = {
-        nodes: nodes.map((node: any) => 
+const requestAndUpdateNetworkGraph = async () => {
+    const result = await lnService.getNetworkGraph({lnd});
+    networkGraphSubject$.next(result);
+    return result;
+}
+
+const mapToFilteredView = (result: any) => {
+    return {
+        nodes: result.nodes.map((node: any) => 
         ({
             pub_key: node.public_key,
             alias: node.alias,
             color: node.color
 
         })),
-        edges: channels.map((edge: any, index: any) => {
+        edges: result.channels.map((edge: any, index: any) => {
             if (!edge?.policies[0]?.public_key || !edge?.policies[1]?.public_key) console.log('Public key missing');
             return {
                 node1_pub: edge.policies[0].public_key,
@@ -40,14 +45,29 @@ app.get( "/", async ( req: any, res: any ) => {
             }
         })
     }
+}
 
-    console.log(filteredView.edges.length);
-    console.log(filteredView.nodes.length);
+app.get( "/", async ( req: any, res: any ) => {
+    requestAndUpdateNetworkGraph();
+    networkGraphSubject$.asObservable().pipe(take(1), filter((a) => !a)).subscribe((request) => {
+        const filteredView = mapToFilteredView(request);
 
-    //const mapper = {}
-    console.log('reques')
-    res.send(JSON.stringify(filteredView));//await lnService.getWalletInfo({lnd}))
-    //res.send( "Hello world!" + counter++ );
+        console.log(filteredView.edges.length);
+        console.log(filteredView.nodes.length);
+    
+        res.send(JSON.stringify(filteredView));
+    });
+    // if (!request){
+    //     request = await requestAndUpdateNetworkGraph();
+    // }
+    // requestAndUpdateNetworkGraph();
+    // console.log(request);
+    // const filteredView = mapToFilteredView(request);
+
+    // console.log(filteredView.edges.length);
+    // console.log(filteredView.nodes.length);
+
+    //res.send(JSON.stringify(filteredView));
 } );
 
 // start the Express server
@@ -56,10 +76,22 @@ app.listen( config.get('port'), () => {
 });
 
 
+networkGraphSubject$.asObservable().subscribe((newValue) => {
+    //console.log('state updated', newValue)
+    wss.emit('graph-update', wss.clients, JSON.stringify(newValue));
+});
+
+wss.on('graph-update', function connection(wsClients: Set<WebSocket>, message) {
+    wsClients.forEach((w) => {
+        console.log('broadcating to ', w)
+        w.send(message)
+    })
+});
+
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
       console.log('received: %s', data);
     });
   
     ws.send('something');
-  });
+});
