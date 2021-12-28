bf6931e030be3c5d7e5c5a6b1acbdac995b3166b
Max Kotlan <max.maxkot@gmail.com>
2021-12-27 21:27:44 -0500

    config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!')
+const lnService = require('ln-service');
+console.log('testing');
+import express from "express";
+import config from 'config';
+import * as lightning from 'lightning';
+import { LndAuthenticationWithMacaroon } from "lightning";
+
+console.log(config.get('macaroon'))
+
+
+const macaroonFromConfig: LndAuthenticationWithMacaroon = config.get('macaroon');
+const {lnd} = lightning.authenticatedLndGrpc(macaroonFromConfig);
+  
+const app = express();
+let counter = 0;
+
+app.get( "/", ( req: any, res: any ) => {
+    res.send( "Hello world!" + counter++ );
+} );
+
+// start the Express server
+app.listen( config.get('port'), () => {
+    console.log( `server started at http://localhost:${ config.get('port') }` );
+} );
