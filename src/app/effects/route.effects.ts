9ca837dadeee84c0f88b77014bfd73c71a0a6e3c
Max Kotlan <max.maxkot@gmail.com>
2022-01-30 17:10:11 -0500

    back button working

--- src/app/effects/route.effects.ts
+++ src/app/effects/route.effects.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { createEffect } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { filter, map, tap, withLatestFrom } from 'rxjs';
+import { filter, map, mergeMap, switchMap, tap, withLatestFrom } from 'rxjs';
 import { GraphState } from '../reducers/graph.reducer';
 import { selectFinalMatcheNodesFromSearch } from '../selectors/graph.selectors';
 import { Location } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
 import * as controlsActions from '../actions/controls.actions';
 
 @Injectable()
@@ -22,7 +22,7 @@ export class RouteEffects {
             this.store$.select(selectFinalMatcheNodesFromSearch).pipe(
                 filter((node) => !!node?.public_key),
                 //tap((node) => this.location.replaceState(node.public_key)),
-                tap((node) => this.router.navigate(['/', node.public_key])),
+                tap((node) => this.router.navigate([node.public_key])),
                 // this.router
             ),
         { dispatch: false },
@@ -40,14 +40,37 @@ export class RouteEffects {
 
     routerChange$ = createEffect(
         () =>
-            this.activatedRoute.params.pipe(
-                map((params) => params['public_key']),
+            this.router.events.pipe(
+                filter((e) => e instanceof NavigationEnd),
+                map(
+                    () =>
+                        // console.log(
+                        this.router.routerState.snapshot.root?.firstChild?.params['public_key'],
+                    // ),
+                ), //this.activatedRoute.snapshot.params['public_key'])),
+                filter((a) => !!a),
+                // switchMap(() =>
+                //     this.activatedRoute.params.pipe(
+                // map((params) => params['public_key']),
+                //tap((c) => console.log(c)),
                 withLatestFrom(this.store$.select(selectFinalMatcheNodesFromSearch)),
                 filter(
                     ([routePubKey, currentSelected]) => currentSelected?.public_key !== routePubKey,
                 ),
                 map(([routePubKey]) => controlsActions.searchGraph({ searchText: routePubKey })),
+                //     ),
+                // ),
             ),
+
+        // this.activatedRoute.params.pipe(
+        //     map((params) => params['public_key']),
+        //     tap((c) => console.log(c)),
+        //     withLatestFrom(this.store$.select(selectFinalMatcheNodesFromSearch)),
+        //     filter(
+        //         ([routePubKey, currentSelected]) => currentSelected?.public_key !== routePubKey,
+        //     ),
+        //     map(([routePubKey]) => controlsActions.searchGraph({ searchText: routePubKey })),
+        // ),
         { dispatch: true },
     );
 }
