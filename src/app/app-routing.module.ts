f8ae109632380a999de910503692dba83856c66b
Max Kotlan <max.maxkot@gmail.com>
2022-01-30 12:29:52 -0500

    showing in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: ':public_key',
+        component: AppComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
