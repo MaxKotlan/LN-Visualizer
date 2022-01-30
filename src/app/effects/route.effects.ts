fe8ffaf2ea5b7d7969579f5a1d99056a58500f1f
Max Kotlan <max.maxkot@gmail.com>
2022-01-30 12:57:42 -0500

    not working

--- src/app/effects/route.effects.ts
+++ src/app/effects/route.effects.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { createEffect } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { filter, tap } from 'rxjs';
+import { filter, map, tap, withLatestFrom } from 'rxjs';
 import { GraphState } from '../reducers/graph.reducer';
 import { selectFinalMatcheNodesFromSearch } from '../selectors/graph.selectors';
 import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as controlsActions from '../actions/controls.actions';
 
 @Injectable()
 export class RouteEffects {
-    constructor(private store$: Store<GraphState>, private location: Location) {}
+    constructor(
+        private store$: Store<GraphState>,
+        private location: Location,
+        private router: ActivatedRoute,
+    ) {}
 
     finalNodeSelected$ = createEffect(
         () =>
@@ -26,5 +32,20 @@ export class RouteEffects {
                 tap(() => this.location.replaceState('')),
             ),
         { dispatch: false },
+    );
+
+    routerChange$ = createEffect(
+        () =>
+            this.router.params.pipe(
+                map((params) => params['public_key']),
+                withLatestFrom(this.store$.select(selectFinalMatcheNodesFromSearch)),
+                filter(
+                    ([routePubKey, currentSelected]) => currentSelected?.public_key !== routePubKey,
+                ),
+                map(([routePubKey, currentSelected]) =>
+                    controlsActions.searchGraph({ searchText: routePubKey }),
+                ),
+            ),
+        { dispatch: true },
     );
 }
