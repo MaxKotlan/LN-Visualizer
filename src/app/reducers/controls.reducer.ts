669f91ca105715606acc2ffaa46059f07e20fc56
Max Kotlan <max.maxkot@gmail.com>
2022-02-13 20:59:22 -0500

    capacity filter

--- src/app/reducers/controls.reducer.ts
+++ src/app/reducers/controls.reducer.ts
@@ -37,7 +37,7 @@ const initialState: ControlsState = {
     cameraFocusMode: CameraFocusMode.FocusOnly,
     edgeDepthTest: false,
     edgeDottedLine: true,
-    capacityFilterEnable: true,
+    capacityFilterEnable: false,
     capacityFilterAmount: 8000000,
 };
 
