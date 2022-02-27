3320f50cdaeea99f744242d1b3217cd218801408
Max Kotlan <max.maxkot@gmail.com>
2022-02-26 21:08:44 -0500

    material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,39 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { AtftModule } from 'atft';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphSceneComponent } from './components/graph-scene/graph-scene.component';
-import { GraphEdgeMeshComponent } from './components/graph-edge-mesh/graph-edge-mesh.component';
-import { GraphNodeMeshComponent } from './components/graph-node-mesh/graph-node-mesh.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { GraphEffects } from './effects/graph.effects';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { SearchComponent } from './components/search/search.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { FormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { GraphFontMeshComponent } from './components/graph-font-mesh/graph-font-mesh.component';
-import { QuickControlsComponent } from './components/quick-controls/quick-controls.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { MatIconModule } from '@angular/material/icon';
-import { SettingsModalComponent } from './components/settings-modal/settings-modal.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
-import { ControlsEffects } from './effects/controls.effects';
-import { ErrorComponent } from './components/error/error.component';
-import { LndRaycasterService } from './services/lnd-raycaster-service';
-import { LndRaycasterEnableDirective } from './directives/lnd-raycaster-enable.directive';
-import { LndRaycasterCameraDirective } from './directives/lnd-raycaster-camera.directive';
-import { LndRaycasterSceneDirective } from './directives/lnd-raycaster-scene.directive';
-import { LoadingBarComponent } from './components/loading-bar/loading-bar.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { GraphMeshStateService } from './services/graph-mesh-state.service';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { GraphStatsComponent } from './components/graph-stats/graph-stats.component';
-import { RouteEffects } from './effects/route.effects';
-import { MatSelectModule } from '@angular/material/select';
 import { CameraSettingsComponent } from './components/camera-settings/camera-settings.component';
 import { ChannelSettingsComponent } from './components/channel-settings/channel-settings.component';
-import { NodeSettingsComponent } from './components/node-settings/node-settings.component';
+import { ErrorComponent } from './components/error/error.component';
+import { GraphEdgeMeshComponent } from './components/graph-edge-mesh/graph-edge-mesh.component';
+import { GraphFontMeshComponent } from './components/graph-font-mesh/graph-font-mesh.component';
+import { GraphNodeMeshComponent } from './components/graph-node-mesh/graph-node-mesh.component';
+import { GraphSceneComponent } from './components/graph-scene/graph-scene.component';
+import { GraphStatsComponent } from './components/graph-stats/graph-stats.component';
+import { LoadingBarComponent } from './components/loading-bar/loading-bar.component';
 import { MiscSettingsComponent } from './components/misc-settings/misc-settings.component';
+import { NodeSettingsComponent } from './components/node-settings/node-settings.component';
+import { QuickControlsComponent } from './components/quick-controls/quick-controls.component';
+import { SearchComponent } from './components/search/search.component';
+import { SettingsModalComponent } from './components/settings-modal/settings-modal.component';
+import { SettingsComponent } from './components/settings/settings.component';
 import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { LndRaycasterCameraDirective } from './directives/lnd-raycaster-camera.directive';
+import { LndRaycasterEnableDirective } from './directives/lnd-raycaster-enable.directive';
+import { LndRaycasterSceneDirective } from './directives/lnd-raycaster-scene.directive';
+import { ControlsEffects } from './effects/controls.effects';
+import { GraphEffects } from './effects/graph.effects';
+import { RouteEffects } from './effects/route.effects';
+import { MaterialModule } from './modules/material';
+import { metaReducers, reducers } from './reducers';
+import { GraphMeshStateService } from './services/graph-mesh-state.service';
+import { LndRaycasterService } from './services/lnd-raycaster-service';
 
 @NgModule({
     declarations: [
@@ -80,6 +65,7 @@ import { TooltipComponent } from './components/tooltip/tooltip.component';
         HttpClientModule,
         AtftModule,
         EffectsModule.forRoot([ControlsEffects, GraphEffects, RouteEffects]),
+        MaterialModule,
         StoreModule.forRoot(reducers, {
             metaReducers,
             runtimeChecks: {
@@ -88,34 +74,6 @@ import { TooltipComponent } from './components/tooltip/tooltip.component';
             },
         }),
         BrowserAnimationsModule,
-        MatInputModule,
-        MatCardModule,
-        MatAutocompleteModule,
-        MatProgressSpinnerModule,
-        MatCheckboxModule,
-        MatSliderModule,
-        MatSlideToggleModule,
-        MatButtonModule,
-        MatIconModule,
-        MatDialogModule,
-        MatToolbarModule,
-        MatDividerModule,
-        MatProgressBarModule,
-        MatSnackBarModule,
-        MatSelectModule,
-        MatTooltipModule,
-        // StoreDevtoolsModule.instrument({
-        //     maxAge: 25,
-        //     logOnly: true,
-        //     autoPause: true,
-        //     features: {
-        //         pause: false,
-        //         lock: true,
-        //         persist: false,
-        //         dispatch: false,
-        //         reorder: false,
-        //     },
-        // }),
     ],
     providers: [LndRaycasterService, GraphMeshStateService],
     bootstrap: [AppComponent],
