a50aa6f76b0340d724b2e484fdebaf479b3694e8
Max Kotlan <max.maxkot@gmail.com>
2022-03-31 17:30:32 -0400

    stats hover

---
+++ src/app/modules/floating-button-menu/components/stats-button/stats-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-button',
+  templateUrl: './stats-button.component.html',
+  styleUrls: ['./stats-button.component.scss']
+})
+export class StatsButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/graph-view/components/graph-view/graph-view.component.ts
+++ src/app/modules/graph-view/components/graph-view/graph-view.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenSizeService } from 'src/app/modules/screen-size/services';
 
 @Component({
-  selector: 'app-graph-view',
-  templateUrl: './graph-view.component.html',
-  styleUrls: ['./graph-view.component.scss']
+    selector: 'app-graph-view',
+    templateUrl: './graph-view.component.html',
+    styleUrls: ['./graph-view.component.scss'],
 })
-export class GraphViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class GraphViewComponent {
+    constructor(public screenSizeService: ScreenSizeService) {}
 }
