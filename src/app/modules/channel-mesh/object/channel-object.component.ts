a29830fec57ceccdd5c4a909a81aa67f988a73c1
Max Kotlan <max.maxkot@gmail.com>
2022-04-14 17:10:19 -0400

    index

--- src/app/modules/channel-mesh/object/channel-object.component.ts
+++ src/app/modules/channel-mesh/object/channel-object.component.ts
@@ -1,9 +1,7 @@
 import { Component, Optional, SkipSelf } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { Store } from '@ngrx/store';
 import { AbstractObject3D, provideParent, RendererService, SphereMeshComponent } from 'atft';
 import * as THREE from 'three';
-import { GraphState } from '../../graph-renderer/reducer';
 import { ChannelGeometry } from '../geometry';
 import { ChannelMaterial } from '../material/channel-material.service';
 
@@ -14,12 +12,8 @@ import { ChannelMaterial } from '../material/channel-material.service';
     template: '<ng-content></ng-content>',
 })
 export class ChannelObjectComponent extends AbstractObject3D<THREE.LineSegments> {
-    dashedLines: boolean = true;
-    depthTest: boolean = true;
-
     constructor(
         protected override rendererService: RendererService,
-        private store$: Store<GraphState>,
         private geometry: ChannelGeometry,
         private material: ChannelMaterial,
         @SkipSelf() @Optional() protected override parent: AbstractObject3D<any>,
