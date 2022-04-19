0d3aeb728fc7bbb0d55f9260cff5210ce5728f36
Max Kotlan <max.maxkot@gmail.com>
2022-04-19 16:48:31 -0400

    t

--- src/app/modules/graph-renderer/services/graph-database/graph-database.service.ts
+++ src/app/modules/graph-renderer/services/graph-database/graph-database.service.ts
@@ -15,17 +15,42 @@ export class GraphDatabaseService {
         private channelRegistry: ChannelRegistryService,
     ) {
         console.log(this.db);
-        this.db.config.debug = true;
+        this.db.config.debug = false;
         this.load();
     }
 
-    public save(channels) {
+    public save() {
         // console.log('Saving', channel);
         // this.dbService.bulkAdd('nodes', Array.from(this.nodeRegistry));
-        // const channels = Array.from(this.channelRegistry.values());
+        const channels = Array.from(this.channelRegistry.values());
+
+        console.log('saving');
+
+        const trimed = channels.map((c) => ({ id: c.id, capacity: c.capacity }));
+        console.log(trimed);
+        console.log(this.db.collection('channels'));
+
+        this.db
+            .collection('channels')
+            .set(trimed)
+            .catch((error) => {
+                console.log('There was an error, do something else.');
+                console.log(error);
+            });
+
+        const a: Promise<any> = this.db.collection('channels').get();
+
+        a.then((c) => console.log('test', c));
+
+        // const chunkSize = 200;
+        // for (let i = 0; i < channels.length; i += chunkSize) {
+        //     const chunk = channels.slice(i, i + chunkSize);
+        //     this.db.collection('channels').update(chunk.map);
+        // }
+
         // console.log(channels);
         // console.log('setting');
-        if (this.shouldLoad) this.db.collection('channels').set(channels);
+        // if (this.shouldLoad) this.db.collection('channels').set(channels);
     }
 
     public shouldLoad = true;
