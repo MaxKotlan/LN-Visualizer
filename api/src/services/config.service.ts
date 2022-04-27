4d22e6942cc6fbd347eb80baa9ddef8d0f503981
Max Kotlan <max.maxkot@gmail.com>
2022-04-27 11:44:37 -0400

    nested in default override

--- api/src/services/config.service.ts
+++ api/src/services/config.service.ts
@@ -16,7 +16,7 @@ const initConfig: Config = {
         learningRate: 1.0,
         logRate: 10,
         maxConnectedNodeDistance: 0.1,
-        minConnectedNodeDistance: 0.1,
+        minConnectedNodeDistance: 0.04,
         invertConnectedRange: true,
         shouldLog: true,
     } as GradientDescentSettings,
@@ -46,13 +46,24 @@ export class ConfigService {
         });
     }
 
+    private overrideConfigVariables(inMemory: Config, file: Config) {
+        Object.entries(inMemory).forEach(([key, value]) => {
+            if (value instanceof Object) {
+                this.overrideConfigVariables(inMemory[key], file[key]);
+            } else {
+                const configValue = file[key];
+                if (configValue) inMemory[key] = file[key];
+            }
+        });
+    }
+
     private _config!: Config;
 
     private readConfig() {
         const configFile = this.getConfigFile();
-        const configFileAndDefault = { ...initConfig, configFile };
-        this.overrideEnvironmentVariables(configFileAndDefault);
-        this._config = configFileAndDefault;
+        this.overrideConfigVariables(initConfig, configFile);
+        this.overrideEnvironmentVariables(initConfig);
+        this._config = initConfig;
     }
 
     public getConfig(): Config {
