1c8054b878537334fb1750376edf50e916f0686e
Max Kotlan <max.maxkot@gmail.com>
2022-05-03 20:07:52 -0400

    threshold closer to shader

--- src/app/modules/node-mesh/services/node-size-offset/node-size-offset.service.ts
+++ src/app/modules/node-mesh/services/node-size-offset/node-size-offset.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { selectNodeSize } from 'src/app/modules/controls-node/selectors/node-controls.selectors';
+import {
+    selectMinimumNodeSize,
+    selectNodeSize,
+} from 'src/app/modules/controls-node/selectors/node-controls.selectors';
 
 @Injectable({
     providedIn: 'root',
 })
 export class NodeSizeOffsetService {
     constructor(private store$: Store) {
-        this.store$.select(selectNodeSize).subscribe((uniformSize) => {
-            this.uniformSize = uniformSize;
-        });
+        this.handleUpdates();
     }
 
-    public uniformSize: number;
+    handleUpdates() {
+        this.store$.select(selectNodeSize).subscribe((uniformSize) => {
+            this.maximumSize = uniformSize;
+        });
+        this.store$
+            .select(selectMinimumNodeSize)
+            .subscribe((minimumSize) => (this.minimumSize = minimumSize));
+    }
+
+    public minimumSize: number;
+    public maximumSize: number;
 
     getPointSize() {
-        return this.uniformSize;
+        return this.maximumSize + this.minimumSize;
     }
 }
