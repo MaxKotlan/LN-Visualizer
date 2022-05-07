a42b197112f091078d658e7f813a80e1c2634476
Max Kotlan <max.maxkot@gmail.com>
2022-05-07 12:06:17 -0400

    tree component display

--- src/app/modules/node-info/components/node-info/node-info.component.ts
+++ src/app/modules/node-info/components/node-info/node-info.component.ts
@@ -4,35 +4,11 @@ import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { map, tap } from 'rxjs';
 import { NodeSearchEffects } from 'src/app/modules/graph-renderer/effects/node-search.effects';
 
-/**
- * Food data with nested structure.
- * Each node has a key and an optional list of children.
- */
 interface KeyValueNode {
     key: string;
     value?: string;
     children?: KeyValueNode[];
 }
-
-const TREE_DATA: KeyValueNode[] = [
-    {
-        key: 'Fruit',
-        children: [{ key: 'Apple' }, { key: 'Banana' }, { key: 'Fruit loops' }],
-    },
-    {
-        key: 'Vegetables',
-        children: [
-            {
-                key: 'Green',
-                children: [{ key: 'Broccoli' }, { key: 'Brussels sprouts' }],
-            },
-            {
-                key: 'Orange',
-                children: [{ key: 'Pumpkins' }, { key: 'Carrots' }],
-            },
-        ],
-    },
-];
 
 @Component({
     selector: 'app-node-info',
@@ -43,9 +19,7 @@ export class NodeInfoComponent {
     treeControl = new NestedTreeControl<KeyValueNode>((node) => node.children);
     dataSource = new MatTreeNestedDataSource<KeyValueNode>();
 
-    constructor(public nodeSearchEffects: NodeSearchEffects) {
-        this.dataSource.data = TREE_DATA;
-    }
+    constructor(public nodeSearchEffects: NodeSearchEffects) {}
 
     public mapObject(object: Object) {
         const r = Object.entries(object);
@@ -60,18 +34,8 @@ export class NodeInfoComponent {
         return { key, value } as KeyValueNode;
     }
 
-    // public mapKeys(([key, value]) => {
-    //   // if (value instanceof Object){
-    //   // }
-
-    // });
-
     public treeData$ = this.nodeSearchEffects.selectFinalMatcheNodesFromSearch$.pipe(
         map((object) => this.mapObject(object)), // {
-        //     // const r = Object.entries(object);
-        //     // return r.map(([key, value]) => ({ key, value }));
-        // }),
-        tap((c) => console.log(c)),
         map((r) => {
             const a = new MatTreeNestedDataSource<KeyValueNode>();
             a.data = r;
