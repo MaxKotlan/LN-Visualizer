9e070cbace929c1b035a2c12ef1589b2b5ea9332
Max Kotlan <max.maxkot@gmail.com>
2022-05-25 22:03:06 -0400

    this code is terrible

--- src/app/modules/unit-conversions/service/unit-converter-service/unit-converter-service.service.ts
+++ src/app/modules/unit-conversions/service/unit-converter-service/unit-converter-service.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
 
+type UnitLabel = 'log' | 'linear' | 'sat' | 'btc';
+
+export interface Unit {
+    value: number;
+    type: UnitLabel;
+    base: 1;
+}
+
+export interface UnitMap {}
+
 @Injectable({
     providedIn: 'root',
 })
-export class UnitConverterService {}
+export class UnitConverterService {
+    public converter(from: Unit, to: UnitLabel): Unit {
+        return this.convertFrom[from.type][to](from.value);
+    }
+
+    public readonly convertFrom = {
+        'log': {
+            'linear': (x) => Math.round(Math.pow(10, x) - 1),
+        },
+        'linear': {
+            'log': (x) => Math.log10(x + 1),
+        },
+        'sat': {
+            'btc': (x) => 10000000 / x,
+        },
+        'btc': {
+            'sat': (x) => x / 10000000,
+        },
+    };
+}
