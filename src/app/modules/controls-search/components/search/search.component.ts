de5cabacc6379707d2a74942450faaf1835fe0f4
Max Kotlan <max.maxkot@gmail.com>
2022-05-27 15:58:13 -0400

    fix search

--- src/app/modules/controls-search/components/search/search.component.ts
+++ src/app/modules/controls-search/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { filter, map, Subject, withLatestFrom } from 'rxjs';
 import { NodeSearchEffects } from 'src/app/modules/graph-renderer/effects/node-search.effects';
 import { GraphState } from 'src/app/modules/graph-renderer/reducer';
 import { searchGraph } from '../../../controls/actions';
@@ -11,9 +12,15 @@ import { searchGraph } from '../../../controls/actions';
 })
 export class SearchComponent {
     constructor(private store$: Store<GraphState>, private nodeSearchEffects: NodeSearchEffects) {}
+    public optionSelected$ = new Subject<void>();
 
     public nodeSearchResults$ = this.nodeSearchEffects.selectNodesSearchResults$;
-    public selectSearchString$ = this.nodeSearchEffects.selectFinalMatchAliasFromSearch$;
+    public selectSearchString$ = this.optionSelected$.pipe(
+        withLatestFrom(
+            this.nodeSearchEffects.selectFinalMatchAliasFromSearch$.pipe(filter((x) => !x)),
+        ),
+        map(([, x]) => x),
+    );
 
     clear() {
         this.store$.dispatch(searchGraph({ searchText: '' }));
@@ -26,6 +33,8 @@ export class SearchComponent {
 
     onOptionSelected(event: any) {
         if (event?.option?.value) {
+            this.optionSelected$.next();
+            console.log(event.option);
             this.store$.dispatch(searchGraph({ searchText: event.option.value }));
         }
     }
