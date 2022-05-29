98012c60d4ab33935cf2cde5f9ac6d2d1cfd09a0
Max Kotlan <max.maxkot@gmail.com>
2022-05-28 21:01:20 -0400

    remove fog again

--- src/app/modules/channel-mesh-thick/geometry/channel-thick-geometry.service.ts
+++ src/app/modules/channel-mesh-thick/geometry/channel-thick-geometry.service.ts
@@ -45,6 +45,7 @@ export class ChannelThickGeometry extends LineSegmentsGeometry {
     }
 
     public updateGeometry() {
+        console.log(this);
         this.updateAttributes();
         this.computeBoundingBox();
         this.computeBoundingSphere();
@@ -57,12 +58,12 @@ export class ChannelThickGeometry extends LineSegmentsGeometry {
         this.channelBufferService.color.onUpdate.subscribe((drawRange) => {
             currentDrawRange = drawRange;
             this.initializeGeometry();
-            this.setDrawRange(0, currentShouldRender ? drawRange : 0);
+            //this.setDrawRange(0, currentShouldRender ? drawRange : 0);
         });
 
         this.store$.select(shouldRenderEdges).subscribe((shouldRender) => {
             currentShouldRender = shouldRender;
-            this.setDrawRange(0, currentShouldRender ? currentDrawRange : 0);
+            //this.setDrawRange(0, currentShouldRender ? currentDrawRange : 0);
         });
     }
 }
