7c861c16a697ab01659d76c934b4d5e1e8851682
Max Kotlan <max.maxkot@gmail.com>
2022-05-28 21:30:15 -0400

    catch

--- src/app/modules/pilot-flags/effects/pilot-flags.effects.ts
+++ src/app/modules/pilot-flags/effects/pilot-flags.effects.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect } from '@ngrx/effects';
-import { map } from 'rxjs';
+import { catchError, map, of } from 'rxjs';
 import { setAllPilotFlags } from '../actions';
 import { PilotFlags } from '../reducer';
 import { PilotFlagApiService } from '../services';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export class PilotFlagEffects {
     constructor(private actions$: Actions, private pilotFlagApiService: PilotFlagApiService) {}
 
     getPilotFlags$ = createEffect(() =>
-        this.pilotFlagApiService
-            .getApiConfig()
-            .pipe(map((pilotFlags: PilotFlags) => setAllPilotFlags({ value: pilotFlags }))),
+        this.pilotFlagApiService.getApiConfig().pipe(
+            map((pilotFlags: PilotFlags) => setAllPilotFlags({ value: pilotFlags })),
+            catchError((e) => {
+                console.warn('could not load remote pilot flags');
+                return of();
+            }),
+        ),
     );
 }
