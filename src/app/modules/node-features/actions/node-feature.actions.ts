ac66f600fd63684094fd98a06a65ea86d648464b
Max Kotlan <max.maxkot@gmail.com>
2022-07-21 16:24:21 -0400

    working

--- src/app/modules/node-features/actions/node-feature.actions.ts
+++ src/app/modules/node-features/actions/node-feature.actions.ts
@@ -1,6 +1,11 @@
 import { createAction, props } from '@ngrx/store';
 import { NodeFeature } from '../reducer/node-features.reducer';
 
+export const enableNodeFeaturesFilter = createAction(
+    '[node-features] enableNodeFeaturesFilter',
+    props<{ isEnabled: boolean }>(),
+);
+
 export const updateFeaturesInView = createAction(
     '[node-features] updateFeaturesInView',
     props<{ newNodeFeatures: NodeFeature[] }>(),
--- src/app/modules/node-features/effects/node-features.effects.ts
+++ src/app/modules/node-features/effects/node-features.effects.ts
@@ -1,21 +1,51 @@
 import { Injectable } from '@angular/core';
-import { createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, combineLatest, filter } from 'rxjs';
+import { map, combineLatest, filter, tap } from 'rxjs';
 import * as filterActions from '../../controls-graph-filter/actions';
 import { NodeFeatureFilter } from '../../filter-templates';
 import { nodeFeaturesFilterEnabled$ } from '../../pilot-flags/selectors';
-import { disabledFeatureBits } from '../selectors/node-features.selectors';
+import {
+    disabledFeatureBits,
+    isNodeFeatureFilterEnabled,
+} from '../selectors/node-features.selectors';
+import * as filterSelectors from '../../controls-graph-filter/selectors/filter.selectors';
+import * as nodeFeatureActions from '../actions/node-feature.actions';
 
 @Injectable()
 export class NodeFeaturesEffects {
+    removeFilte$ = createEffect(
+        () =>
+            combineLatest([
+                this.actions$.pipe(ofType(nodeFeatureActions.enableNodeFeaturesFilter)),
+                this.store$.select(nodeFeaturesFilterEnabled$),
+            ]).pipe(
+                tap((x) => console.log(x)),
+                filter(
+                    ([switchEnabled, pilotFlagEnabled]) =>
+                        !switchEnabled.isEnabled || !pilotFlagEnabled,
+                ),
+                map(() =>
+                    filterActions.removeChannelFilterByIssueId({
+                        issueId: this.nodeFeatureFilter.issueId,
+                    }),
+                ),
+                tap((x) => console.log(x)),
+            ),
+        { dispatch: true },
+    );
+
     nodeFeatureScript$ = createEffect(
         () =>
             combineLatest([
                 this.store$.select(disabledFeatureBits),
+                this.actions$.pipe(ofType(nodeFeatureActions.enableNodeFeaturesFilter)),
                 this.store$.select(nodeFeaturesFilterEnabled$),
             ]).pipe(
-                filter(([, pilotFlagEnabled]) => pilotFlagEnabled),
+                filter(
+                    ([, switchEnabled, pilotFlagEnabled]) =>
+                        switchEnabled.isEnabled && pilotFlagEnabled,
+                ),
                 map(([disabledFeatureBits]) => {
                     return filterActions.updateNodeFilterByIssueId({
                         value: this.nodeFeatureFilter.createFilter(disabledFeatureBits),
@@ -25,5 +55,16 @@ export class NodeFeaturesEffects {
         { dispatch: true },
     );
 
-    constructor(private store$: Store, private nodeFeatureFilter: NodeFeatureFilter) {}
+    filterChanged$ = createEffect(() =>
+        this.store$.select(filterSelectors.isFilterActive(this.nodeFeatureFilter.issueId)).pipe(
+            filter((isEnabled) => !isEnabled),
+            map(() => nodeFeatureActions.enableNodeFeaturesFilter({ isEnabled: false })),
+        ),
+    );
+
+    constructor(
+        private actions$: Actions,
+        private store$: Store,
+        private nodeFeatureFilter: NodeFeatureFilter,
+    ) {}
 }
